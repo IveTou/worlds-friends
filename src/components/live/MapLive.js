e84be1e405925931d12c6bf6fdcaaa2c41d2988d
Ive <ive.tourinho@itmov.com.br>
2019-03-20 10:30:43 -0300

    From google-map-react to react-gmaps

--- src/components/live/MapLive.js
+++ src/components/live/MapLive.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import { withStyles } from '@material-ui/core/styles';
+import className from 'classnames';
+import Avatar from '@material-ui/core/Avatar';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+import deepPurple from '@material-ui/core/colors/deepPurple';
 
-const Pin = ({ content, id }) => <div id={id}>{content}</div>;
+const Pin = ({ content, id, classes }) => 
+  <div id={`user-${id}`}>
+    <Avatar 
+      className={className(classes.avatar, classes.orangeAvatar)}
+    >
+      {content}
+    </Avatar>
+  </div>;
 
-const MapLive = ({ initConfig, users = [] }) => {
+const styles = {
+  avatar: {
+    margin: 10,
+  },
+  orangeAvatar: {
+    margin: 10,
+    color: '#fff',
+    backgroundColor: deepOrange[500],
+  },
+  purpleAvatar: {
+    margin: 10,
+    color: '#fff',
+    backgroundColor: deepPurple[500],
+  },
+};
+
+const MapLive = ({ classes, initConfig, users = [] }) => {
   return (
     <GoogleMapReact
       bootstrapURLKeys={{ key: initConfig.key }}
@@ -14,13 +42,14 @@ const MapLive = ({ initConfig, users = [] }) => {
         <Pin 
           id={user.key}
           key={user.key}
+          content={user.value.initials}
+          classes={classes}
           lat={user.value.coordinates && user.value.coordinates.latitude}
           lng={user.value.coordinates && user.value.coordinates.longitude}
-          content={user.value.initials}
         />
       )}
     </GoogleMapReact>
   )
 }
 
-export default MapLive;
+export default withStyles(styles)(MapLive);
