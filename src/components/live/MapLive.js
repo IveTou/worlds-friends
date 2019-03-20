1de8dbeb1f194b4b82a0b20afb814344f31e0b66
Ive <ive.tourinho@itmov.com.br>
2019-03-20 13:07:25 -0300

    Finallizing the branch

--- src/components/live/MapLive.js
+++ src/components/live/MapLive.js
@@ -8,30 +8,35 @@ import {
 } from "react-google-maps";
 import { config } from '../../config/gmConfig';
 
-const MapLive = compose(
+const MapLive = ({ users }) => (
+  <GoogleMap 
+    defaultZoom={config.zoom} 
+    defaultCenter={
+      { 
+        lat: config.center.lat, 
+        lng: config.center.lng,
+      }
+    }>
+    {users && users.map(user =>
+      <Marker 
+        position={{ 
+          lat: user.value.coordinates && user.value.coordinates.latitude, 
+          lng: user.value.coordinates && user.value.coordinates.longitude
+        }} 
+        key={user.key}
+        icon='https://firebasestorage.googleapis.com/v0/b/react-redux-firebase-2f3ee.appspot.com/o/img%2Fpin-green.png?alt=media&token=1b61cb01-48f2-4123-a33b-3b0e0b9b5043'
+        />
+    )}
+  </GoogleMap>
+);
+
+export default compose(
   withProps({
-    googleMapURL: config.url+'key='+config.key+'&v='+config.v,
+    googleMapURL: `${config.url}key=${config.key}&v=${config.v}`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
   withGoogleMap
-  )(({ users, classes }) => (
-    <GoogleMap defaultZoom={8} defaultCenter={
-      { lat: config.center.lat, lng: config.center.lng }
-    }>
-      {users && users.map(user =>
-        <Marker 
-          position={{ 
-            lat: user.value.coordinates && user.value.coordinates.latitude, 
-            lng: user.value.coordinates && user.value.coordinates.longitude
-          }} 
-          key={user.key}
-          icon='https://firebasestorage.googleapis.com/v0/b/react-redux-firebase-2f3ee.appspot.com/o/img%2Fpin-green.png?alt=media&token=1b61cb01-48f2-4123-a33b-3b0e0b9b5043'
-          />
-      )}
-    </GoogleMap>
-));
-
-export default MapLive;
+)(MapLive);
--- src/config/gmConfig.js
+++ src/config/gmConfig.js
@@ -3,12 +3,12 @@ import googleMaps from '@google/maps';
 export const config = {
   url: 'https://maps.googleapis.com/maps/api/js?',
   key: 'AIzaSyBb7bGZ2aw7QRoBgFfl25kxHDwABpjgqDI',
+  v: '3.exp',
   center: {
     lat: -12.98,
     lng: -38.47,
   },
-  zoom: 11,
-  v: '3.exp',
+  zoom: 13,
 }
 
 export default googleMaps.createClient(config);
